8984c6e9f86824bef69fe9f3849efb5521a5a0ef
Daniel Españadero <developer@daniel-espanadero.com>
2022-05-04 03:05:16 +0200

    Products controller finished

--- src/controllers/producst.controller.js
+++ src/controllers/producst.controller.js
@@ -2,9 +2,9 @@ import Product from '../models/Products';
 
 export const createProduct = async (req, res) => {
 
-    const {name, category, price, imgURL} = req.body;
+    const { name, category, price, imgURL } = req.body;
 
-    const newProduct = new Product({name, category, price, imgURL});
+    const newProduct = new Product({ name, category, price, imgURL });
 
     const productSaved = await newProduct.save();
 
@@ -16,7 +16,7 @@ export const createProduct = async (req, res) => {
 export const getProducts = async (req, res) => {
 
     const products = await Product.find();
-    
+
     res.status(200).json({
         products
     })
@@ -30,10 +30,16 @@ export const getProductById = async (req, res) => {
     })
 };
 
-export const updateProductById = (req, res) => {
+export const updateProductById = async (req, res) => {
+    const updatedProduct = await Product.findByIdAndUpdate(req.params.productId, req.body, { new: true });
 
+    res.status(200).json(updatedProduct);
 };
 
-export const deleteProductById = (req, res) => {
+export const deleteProductById = async (req, res) => {
+    await Product.findByIdAndDelete(req.params.productId);
 
+    res.status(204).json({
+        msg: 'Product deleted'
+    });
 };
