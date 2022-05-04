4dd5f5be7bb07ecdd39c6993625cdb46163083c9
Daniel Españadero <developer@daniel-espanadero.com>
2022-05-04 05:11:46 +0200

    signup

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,13 +1,30 @@
 import User from "../models/User";
+import jsonwebtoken from "jsonwebtoken";
+import config from "../config";
 
-export const signUp = (req, res) => {
+export const signUp = async (req, res) => {
+
+    const { username, email, password, roles } = req.body;
+
+    const newUser = new User({
+        username,
+        email,
+        password: await User.encryptPassword(password),
+    });
+
+    const saveUser = await newUser.save();
+
+    const token = jsonwebtoken.sign({id: saveUser._id}, config.SECRET, {
+        expiresIn: '4h'
+    });
+
     res.status(200).json({
-        msg: 'Sign Up'
-    })
+        token
+    });
 };
 
 export const signIn = (req, res) => {
     res.status(200).json({
         msg: 'Sign In'
-    })
+    });
 };
--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import bcryptjs from 'bcryptjs';
 
 const UserSchema = new Schema({
     username: {
@@ -18,9 +19,18 @@ const UserSchema = new Schema({
         type: Schema.Types.ObjectId
     }]
 },
-{
-    timestamps: true,
-    versionKey: false
-});
+    {
+        timestamps: true,
+        versionKey: false
+    });
+
+UserSchema.statics.encryptPassword = async (password) => {
+    const salt = await bcryptjs.genSalt(10);
+    return await bcryptjs.hash(password, salt)
+}
+
+UserSchema.statics.comparePassword = async (password, receivedPassword) => {
+    return await bcryptjs.compare(password, receivedPassword);
+};
 
 export default model('User', UserSchema);
